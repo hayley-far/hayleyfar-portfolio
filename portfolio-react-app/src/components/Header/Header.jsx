0fe41c02c21b9602b312bb2979d5018e615457c0
hayley-far <hayleyfar5@gmail.com>
2025-12-16 20:07:38 +1300

    feat: basic header layout

---
+++ portfolio-react-app/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.module.css';
+import logo from '../../assets/logo.jpeg';
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header>
+        <nav>
+            <a href="#home">Home</a>
+            <a href="#projects">Projects</a>
+        </nav>
+        <img src={logo} alt="Logo" className={styles.logo}/>
+        <h3>hayley's portfolio</h3>
+    </header>
+  );
+}
+export default Header;
