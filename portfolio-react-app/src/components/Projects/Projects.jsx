28c0a29f453d2286e9b80064172a078a83872785
hayley-far <hayleyfar5@gmail.com>
2026-01-24 19:24:38 +1300

    feat(contactMe): layout for contact me section
    with working links

--- portfolio-react-app/src/components/Projects/Projects.jsx
+++ portfolio-react-app/src/components/Projects/Projects.jsx
@@ -10,12 +10,13 @@ function Projects() {
   return (
     <div className={styles.projectsSection}>
       {/* Title above carousel */}
-      <h2 className={styles.projectsTitle}>Projects</h2>
+      <h2>Projects</h2>
 
       {/* Carousel container */}
       <div className={styles.carouselContainer}>
         <Card title="Autonomous Vehicle Challenge" description={<p>ENGR101 Robot Project</p>} variant="projectCard" />
         <Card title="Galaga Game" description={<p>CGRA151 Project</p>} variant="projectCard" />
+        <Card title="Recapp" description={<p>VUW Hackathon 2025</p>} variant="projectCard" />
         <Card title="Fash and Chaps" description={<p>SWEN225 Chips Challenge Game</p>} variant="projectCard" />
         <Card title="Bomberman Game" description={<p>Personal Game Project</p>} variant="projectCard" />
         <Card title="Portfolio Website" description={<p>This website :)</p>} variant="projectCard" />
